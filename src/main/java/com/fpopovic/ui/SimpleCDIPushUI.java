fb5baf3c4442bfa2ab44469dda4ac37adbbc9b0b
Filip Popovic <fpopovic@hotmail.com>
2015-03-12 00:32:51 +0100

    responsive sandbox

--- src/main/java/com/fpopovic/ui/SimpleCDIPushUI.java
+++ src/main/java/com/fpopovic/ui/SimpleCDIPushUI.java
@@ -5,6 +5,8 @@ import com.fpopovic.broadcaster.BroadcasterListener;
 import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
 import com.vaadin.cdi.CDIUI;
+import com.vaadin.cdi.CDIViewProvider;
+import com.vaadin.server.Responsive;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.*;
 import com.vaadin.ui.Button.ClickEvent;
@@ -21,12 +23,18 @@ public class SimpleCDIPushUI extends UI implements BroadcasterListener
     @Inject
     private Broadcaster broadcaster;
 
+    @Inject
+    private CDIViewProvider cdiViewProvider;
+
+
     @Override
     protected void init(VaadinRequest request) {
         System.out.println("started");
         broadcaster.addListener(this);
+
         final VerticalLayout layout = new VerticalLayout();
-        layout.setMargin(true);
+        layout.setSizeFull();
+        Responsive.makeResponsive(layout);
         setContent(layout);
         
         Button button = new Button("Click Me");
@@ -36,7 +44,31 @@ public class SimpleCDIPushUI extends UI implements BroadcasterListener
                 broadcaster.sendMessage("group1","mess1");
             }
         });
-        layout.addComponent(button);
+        //layout.addComponent(button);
+
+        CssLayout cssLayout = new CssLayout();
+        cssLayout.addStyleName("example-css-layout");
+        cssLayout.setSizeFull();
+        AbsoluteLayout absoluteLayout = new AbsoluteLayout();
+        absoluteLayout.addStyleName("example-absolute-layout");
+        absoluteLayout.setSizeFull();
+        Responsive.makeResponsive(cssLayout);
+        Responsive.makeResponsive(absoluteLayout);
+
+        Label cssLabel = new Label("This is CssLayout Label");
+        cssLabel.addStyleName("example-css-label");
+        cssLabel.setSizeUndefined();
+        Label absoluteLabel = new Label("This is Absolute Label");
+        absoluteLabel.addStyleName("example-absolute-label");
+        absoluteLabel.setSizeUndefined();
+        Responsive.makeResponsive(cssLabel);
+        Responsive.makeResponsive(absoluteLabel);
+
+        cssLayout.addComponent(cssLabel);
+        absoluteLayout.addComponent(absoluteLabel,"top:20px;right:50px;");
+
+        layout.addComponent(cssLayout);
+        layout.addComponent(absoluteLayout);
     }
 
 
@@ -49,4 +81,6 @@ public class SimpleCDIPushUI extends UI implements BroadcasterListener
                 }
             });
         }
+
+
 }
