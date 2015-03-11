47b48ba866e65ea9a2afe29b20edccf14b0a8ca4
Filip Popovic <fpopovic@hotmail.com>
2015-03-12 00:32:51 +0100

    responsive sandbox

---
+++ src/main/java/com/fpopovic/view/ExampleView.java
@@ -0,0 +1,18 @@
+package com.fpopovic.view;
+
+import com.vaadin.cdi.CDIView;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+
+/**
+ * @author <A HREF="mailto:fpopovic@hotmail.com">Filip Popovic</A>
+ */
+@CDIView
+public class ExampleView implements View{
+
+
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent event) {
+
+    }
+}
